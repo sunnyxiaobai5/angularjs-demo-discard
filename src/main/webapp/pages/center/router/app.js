a6f830181dc84d85aa6ef4a89f02d640f15f1b54
sunnyxiaobai5 <sunnyxiaobai5@gmail.com>
2015-07-04 17:10:51 +0800

    命名ui-view测试

--- src/main/webapp/pages/center/router/app.js
+++ src/main/webapp/pages/center/router/app.js
@@ -11,7 +11,18 @@ angular.module("routerApp", ["ui.router"]).config(function ($stateProvider, $url
     $stateProvider
         .state("page1", {
             url: "/page1",
-            templateUrl: "/pages/center/router/slidbar1.htm"
+            //templateUrl: "/pages/center/router/slidbar1.htm",
+            views: {
+                "": {
+                    templateUrl: "/pages/center/router/slidbar1.htm"
+                },
+                "banner": {
+                    templateUrl: "/pages/center/router/common/banner.htm"
+                },
+                "footer": {
+                    templateUrl: "/pages/center/router/common/footer.htm"
+                }
+            }
         })
         .state("page2", {
             url: "/page2",
@@ -22,21 +33,21 @@ angular.module("routerApp", ["ui.router"]).config(function ($stateProvider, $url
             templateUrl: "/pages/center/router/slidbar1_content1.htm"
         })
         .state("page1.content2", {
-            url: "/content1",
+            url: "/content2",
             templateUrl: "/pages/center/router/slidbar1_content2.htm"
         })
         .state("page1.content3", {
-            url: "/content1",
+            url: "/content3",
             templateUrl: "/pages/center/router/slidbar1_content3.htm"
         })
         .state("page2.content1", {
             url: "/content1",
             templateUrl: "/pages/center/router/slidbar2_content1.htm"
         }).state("page2.content2", {
-            url: "/content1",
+            url: "/content2",
             templateUrl: "/pages/center/router/slidbar2_content2.htm"
         }).state("page2.content3", {
-            url: "/content1",
+            url: "/content3",
             templateUrl: "/pages/center/router/slidbar2_content3.htm"
         })
 });
